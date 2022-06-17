9c98a42cd271bdcaf7bf228c0866a0f870b35dbf
jeanHoffelder <jl6934807@outlook.com>
2022-06-17 08:49:29 -0300

    upload

--- a.js
+++ a.js
@@ -1,20 +1,16 @@
 import './biblioteca';
 document.addEventListener("DOMContentLoaded", function(event) {
-    
-    var dark = document.querySelector('#dark');
-var light = document.querySelector('#light');
-var cont = document.querySelector('#container');
 
-function Dark(){
-    cont.style.color="white"
-    cont.style.backgroundColor="black"
-    document.body.style.backgroundImage="url('img/wpb.jpg')"
-}
-function Light(){
-    cont.style.color="black"
-    cont.style.backgroundColor="white"
-    document.body.style.backgroundImage="url('img/wp.jpg')"
-}
-dark.addEventListener("click", Dark);
-light.addEventListener("click", Light);
+
+function darkMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+  }
+  function lightMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+  }
+
 })
