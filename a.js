e75b966ca22bed07dbf0e361e7f138b6c7bf792a
jeanHoffelder <jl6934807@outlook.com>
2022-06-22 23:05:18 -0300

    up

--- a.js
+++ a.js
@@ -10,10 +10,53 @@ const TRHCP = [
   "Police Helicopter",
   "You Always Sing The Same",
   "Grand Pappy Du Plenty",
-]
-const chk = document.getElementById('chk')
 
-chk.addEventListener('change', () => {
+  "Hollywood (Africa)",
+  "American Ghost Dance",
+  "If You Want Me To Stay",
+  "Nevermind",
+  "Freaky Styley",
+  "Blackeyed Blonde",
+  "The Brothers Cup",
+  "Battle Ship",
+  "Lovin' And Touchin'",
+  "Catholic School Girls Rule",
+  "Sex Rap",
+  "Thirty Dirty Birds",
+  "Yertle The Turtle",
+  "Nevermind",
+  "Sex Rap",
+  "Millionaires Against Hunger",
+
+  
+]
+const mode = document.getElementById('mode')
+mode.addEventListener('change', () => {
   document.body.classList.toggle('dark')
+})
+
+
+const btn = document.querySelector('#revela')
+
+
+
+const nao = ("O NÚMERO DO DIA DEVE ESTAR ENTRE 1 E 31")
+var result = document.getElementById("musica");
+
+
+btn.addEventListener("click" , function() {
+  const dia = document.querySelector('#dia');
+  const numdia = dia.value;
+  
+  if(numdia>31,  numdia<1){
+    result.innerHTML=(nao);
+  }else{ 
+    const select = document.getElementById('mes').value;
+    var numusica = select * numdia
+    result.innerHTML=(TRHCP[numusica]);
+
+
+  
+  }
   
 })
